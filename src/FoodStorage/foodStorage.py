6b14ff52e36ef79092421e0edffff10bf34da9fc
Amanda Te <amandate@Amandas-MacBook-Pro.local>
2022-11-08 22:33:01 -0700

    update add methods

--- src/FoodStorage/foodStorage.py
+++ src/FoodStorage/foodStorage.py
@@ -5,11 +5,22 @@ class FoodStorage:
         self.isOpen = False
         self.__inventory__ = PriorityQueue()
 
+    ''' Adds each food item from list_of_foods to self.__inventory__ '''
+    def addFoods(self, list_of_foods):
+        for food in list_of_foods:
+            self.add(food)
+        print("Successfully added all foods: ${list_of_foods}")
+
+    ''' Adds food item to Food Storage. '''
     def add(self, food):
-        return
+        date = food.expiration_date
+        self.__inventory__.put((date, food))
 
     def remove(self, food):
         return
+
+    def list(self):
+        return list(self.__inventory__)
     
     def update(self, food):
         return
