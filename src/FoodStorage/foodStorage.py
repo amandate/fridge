e7f7c9b3e404379a9225afb00ea16d9294739db5
amandate <amandate1997@berkeley.edu>
2022-11-09 23:04:53 -0700

    renamed priv meths/vars

--- src/FoodStorage/foodStorage.py
+++ src/FoodStorage/foodStorage.py
@@ -3,39 +3,39 @@ from queue import PriorityQueue
 class FoodStorage:
     def __init__(self):
         self.isOpen = False
-        self.__inventoryPQ__ = PriorityQueue()
-        self.__sortedInventory__ = []
+        self._inventoryPQ = PriorityQueue()
+        self._sortedInventory = []
 
-    ''' Adds each food item from list_of_foods to self.__inventoryPQ__ '''
+    ''' Adds each food item from list_of_foods to self._inventoryPQ '''
     def addFoods(self, list_of_foods):
         for food in list_of_foods:
             self.add(food)
         print("Successfully added all foods: ${list_of_foods}")
 
-    ''' Adds food item to self.__inventoryPQ__. '''
+    ''' Adds food item to self._inventoryPQ. '''
     def add(self, food):
         date = food.expiration_date
-        self.__inventoryPQ__.put((date, food))
+        self._inventoryPQ.put((date, food))
 
     def remove(self, food):
         return
 
-    ''' Updates self.__sortedInventory__. If there were new foods added, since we last 
-        updated our self.__sortedInventory__, we add everything from the current 
-        self.__sortedInventory__ into self.__inventoryPQ__ and use it to repopulate 
-        self.__sortedInventory___. '''
-    def __sortInventory__(self):
-        if not self.__inventoryPQ__.empty():
-            while self.__sortedInventory__:
-                self.__inventoryPQ__.put(self.__sortedInventory__.pop())
+    ''' Updates self._sortedInventory. If there were new foods added, since we last 
+        updated our self._sortedInventory, we add everything from the current 
+        self._sortedInventory into self._inventoryPQ and use it to repopulate 
+        self._sortedInventory_. '''
+    def _sortInventory(self):
+        if not self._inventoryPQ.empty():
+            while self._sortedInventory:
+                self._inventoryPQ.put(self._sortedInventory.pop())
 
-            while not self.__inventoryPQ__.empty():
-                self.__sortedInventory__.append(self.__inventoryPQ__.get())
+            while not self._inventoryPQ.empty():
+                self._sortedInventory.append(self._inventoryPQ.get())
 
     ''' Lists our inventory in order of earliest expiration date. '''
     def list(self):
         self.__sortInventory__()
-        return self.__sortedInventory__
+        return self._sortedInventory
     
     def update(self, food):
         return
