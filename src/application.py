279ee758e9d094c693e19cecffd9fe0ea54008d6
amandate <amandate1997@berkeley.edu>
2022-11-11 16:35:58 -0700

    WIP

--- src/application.py
+++ src/application.py
@@ -2,14 +2,26 @@ from src.Food.food import Food
 from src.FoodStorage.fridge import Fridge
 from src.FoodStorage.freezer import Freezer
 
+# Steps:
+# File to handle commands
+# File to create profile w/ profile_name, curr_date, map of names to food storages
+#   - profile
+#       - profile_name.json
+#           - { "fridge1": [{"name": "apple", "expiration": "2022-11-08", "amt": 1}, ...]
+#                   }
+# File to reconstruct profiles
+
+
 add_food = 'add food'
 create_freezer = 'create freezer'
 create_fridge = 'create fridge'
+create_profile = 'create profile'
 list_command = 'list'
 list_food_storages = 'list food storages'
 list_fridges = 'list fridges'
 list_freezers = 'list freezers'
 open = 'open'
+save = "save"
 
 def _listCommands():
     print(create_freezer, create_fridge, list_command, sep = "\n")
