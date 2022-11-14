749952a580bf5139f74a4f12976af79a6021f790
leezahuor <leezahuor@gmail.com>
2022-11-14 14:58:49 -0700

    Added open function to food object

--- src/Food/food.py
+++ src/Food/food.py
@@ -1,9 +1,16 @@
+from datetime import date, timedelta
+
 class Food(object):
     def __init__(self, name, expiration_date, use_by_length):
         self.name = name
-        self.expiration_date = expiration_date
-        self.use_by_length = use_by_length
+        self.expiration_date = date(expiration_date)
+        self.use_by_length = timedelta(days = use_by_length)
+        self.isOpen = False
     
-    def openItem():
-        #update expiration date with use by date 
+    '''Updates self.expiration_date to self.use_by_length according to date food item is opened. 
+        If date is changed, self.isOpen updates to True.'''
+    def open(self):
+        if not self.isOpen:
+            self.expiration_date = date.today() + self.use_by_length
+            self.isOpen = True
         
--- tests/Food /test_food.py
+++ tests/Food /test_food.py
@@ -8,7 +8,16 @@ class Test_Food(unittest.TestCase):
         self.assertEqual("apple", food.name)
         self.assertEqual("2022-5-4", food.expiration_date)
         self.assertEqual(2, food.use_by_length)
+        self.assertFalse(self.food.isOpen) 
+    
+    def testOpenFoods(self):
+
+
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+#conduct tests for new function 
