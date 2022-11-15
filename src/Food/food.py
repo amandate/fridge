a47699f84b5ba850f9a8607b4e10ebb3a4f35cb3
leezahuor <leezahuor@gmail.com>
2022-11-15 11:41:28 -0700

    added comments and minor edits

--- src/Food/food.py
+++ src/Food/food.py
@@ -1,16 +1,17 @@
 from datetime import date, timedelta
 
 class Food(object):
-    def __init__(self, name, expiration_date, use_by_length):
+    def __init__(self, name, expiration_date, use_by_date):
         self.name = name
         self.expiration_date = date.fromisoformat(expiration_date)
-        self.use_by_length = timedelta(days = use_by_length)
+        
+        # use_by_date indicates the amount of time a food's quality will deteriorate after it is opened and suggests the food item should be consumed in that given timeframe.
+        self.use_by_date = timedelta(days = use_by_date)
         self.isOpen = False
     
-    '''Updates self.expiration_date to self.use_by_length according to date food item is opened. 
-        If date is changed, self.isOpen updates to True.'''
+    '''Updates self.expiration_date to the current date plus self.use_by_date and updates self.isOpen. '''
     def open(self):
         if not self.isOpen:
-            self.expiration_date = date.today() + self.use_by_length
+            self.expiration_date = date.today() + self.use_by_date
             self.isOpen = True
         
--- tests/Food /test_food.py
+++ tests/Food /test_food.py
@@ -10,12 +10,14 @@ class Test_Food(unittest.TestCase):
     def testInitializeFood(self):
         self.assertEqual("apple", self.food.name)
         self.assertEqual(date(2022, 5, 4), self.food.expiration_date)
-        self.assertEqual(timedelta(days = 2), self.food.use_by_length)
+        self.assertEqual(timedelta(days = 2), self.food.use_by_date)
         self.assertFalse(self.food.isOpen)  
     
     def testOpenFoods(self): 
+        # Checks the initial value of isOpen, food should not be open yet.
         self.assertFalse(self.food.isOpen)
 
+        # Opens food and updates expiration_date to new use_by_date. 
         self.food.open()
         self.assertEqual(date.today() + timedelta(days = 2), self.food.expiration_date)
         self.assertTrue(self.food.isOpen)
