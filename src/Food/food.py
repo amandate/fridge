081b212a90cb772645b6ad8f8566710ffc450dce
leezahuor <leezahuor@gmail.com>
2022-11-16 10:32:03 -0700

    added food comparator and corresponding tests

--- src/Food/food.py
+++ src/Food/food.py
@@ -8,10 +8,36 @@ class Food(object):
         # use_by_date indicates the amount of time (in days) a food's quality will deteriorate after it is opened
         self.use_by_date = timedelta(days = use_by_date)
         self.isOpen = False
-    
+
+    def __lt__(self, other): 
+        if self.expiration_date == other.expiration_date:
+            return self.name < other.name
+        return self.expiration_date < other.expiration_date
+            
+    def __le__(self, other):
+        if self.expiration_date == other.expiration_date:
+            return self.name <= other.name
+        return self.expiration_date <= other.expiration_date
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __ne__(self, other):
+        return self.name != other.name
+
+    def __gt__(self, other):
+        if self.expiration_date == other.expiration_date:
+            return self.name > other.name
+        return self.expiration_date > other.expiration_date
+
+    def __ge__(self, other):
+        if self.expiration_date == other.expiration_date:
+            return self.name >= other.name
+        return self.expiration_date >= other.expiration_date
+
     '''Updates self.expiration_date to the current date plus self.use_by_date and updates self.isOpen. '''
     def open(self):
         if not self.isOpen:
             self.expiration_date = date.today() + self.use_by_date
             self.isOpen = True
-        
+    
--- tests/Food /test_food.py
+++ tests/Food /test_food.py
@@ -1,6 +1,8 @@
-import unittest
 from datetime import date, timedelta
 from src.Food.food import Food
+from tests.Utilities.constants import *
+
+import unittest
 
 class Test_Food(unittest.TestCase):
    
@@ -13,6 +15,23 @@ class Test_Food(unittest.TestCase):
         self.assertEqual(timedelta(days = 2), self.food.use_by_date)
         self.assertFalse(self.food.isOpen)  
     
+    def testCompareFoods(self):
+        self.assertLess(food1, food2) # date1 < date2
+        self.assertLess(food1, food4) # date1 = date4
+        self.assertLessEqual(food1, food4) # date1 = date4
+        self.assertLessEqual(food1, food2) # date1 < date2
+        self.assertEqual(food1, food5) # name1, date1 = name5, date5
+        self.assertNotEqual(food1, food6) # name1 != name6, date1 = date6
+        self.assertNotEqual(food1, food2) # name1 != name2, date1 != date 2
+        self.assertGreaterEqual(food4, food1) # date4 = date1
+        self.assertGreaterEqual(food2, food1) # date2 > date1
+        self.assertGreater(food2, food1) # date2 > date1
+        self.assertGreater(food4, food1) # date4 = date1
+
+        actual = sorted([food1, food6, food3, food2, food5, food4])
+        expected = [food3, food1, food5, food6, food4, food2]
+        self.assertEqual(expected, actual)
+
     def testOpenFoods(self): 
         # Checks the initial value of isOpen, food should not be open yet.
         self.assertFalse(self.food.isOpen)
