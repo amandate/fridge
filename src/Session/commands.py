c02b2ce08bf3e8f09b5b30f0a1361aaf5c3f4529
leezahuor <leezahuor@gmail.com>
2022-11-21 11:05:50 -0800

    implementing commands class

---
+++ src/Session/commands.py
@@ -0,0 +1,48 @@
+class Commands:
+    def __init__(self):
+        pass
+
+    def add_food(self):
+        pass
+
+    def remove_food(self):
+        pass
+
+    def create_freezer(self):
+        pass
+
+    def create_fridge(self):
+        pass
+
+    def create_profile(self):
+        pass
+
+    def help(self):
+        pass
+
+    def list_food_storages(self):
+        pass
+
+    def list_fridges(self):
+        pass
+
+    def list_freezers(self):
+        pass
+
+    def load(self):
+        pass
+    
+    def open(self):
+        pass
+
+    def save(self):
+        pass
+
+    def delete_fridge(self):
+        pass
+
+    def delete_freezer(self):
+        pass
+    
+    def delete_profile(self):
+        pass
