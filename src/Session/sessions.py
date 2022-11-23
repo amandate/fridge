f9e705a0c8d3469ed1ca06e9006d8e41c5c83cda
leezahuor <leezahuor@gmail.com>
2022-11-23 11:02:16 -0800

    adding additional methods and some edits

--- src/Session/sessions.py
+++ src/Session/sessions.py
@@ -1,3 +1,14 @@
 class Sessions:
-    def __init__(self, profile):
+    def __init__(self, None):
         pass
+    
+    def redirect(self):
+        pass
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass 
+
+
