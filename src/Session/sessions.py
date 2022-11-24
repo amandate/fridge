08539bbe7100ba92d98e89f8be69b3e36f70c335
leezahuor <leezahuor@gmail.com>
2022-11-23 17:27:34 -0800

    removed none and added user_input

--- src/Session/sessions.py
+++ src/Session/sessions.py
@@ -1,8 +1,8 @@
 class Sessions:
-    def __init__(self, None):
+    def __init__(self):
         pass
     
-    def redirect(self):
+    def redirect(self, user_input):
         pass
 
     def start(self):
