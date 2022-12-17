9bdd8df314a8c461d83dcb4d48d0fcb262bc7e5b
leezahuor <leezahuor@gmail.com>
2022-12-16 16:29:54 -0800

    consolidated printwelcomemessage

--- src/Session/session.py
+++ src/Session/session.py
@@ -1,15 +1,12 @@
-from os import listdir
-from src.Constants.commands_messages import \
-    SUGGESTED_ACTIONS_MESSAGE
+from os import listdir    
 from src.Constants.constants import \
     CREATE_PROFILE, \
     EMPTY_STRING, \
     EXIT, \
     JSON_EXTENSION, \
-    PROFILES_PATH, \
-    WITHIN_QUOTES
+    PROFILES_PATH
 from src.Constants.session_messages import *
-from src.Utils.utils import listSepByTab, twoPrintOutcomes
+from src.Utils.utils import twoPrintOutcomes
 
 class Session:
     def __init__(self):
@@ -19,20 +16,14 @@ class Session:
     def _getSavedProfiles(self):
         return [file.replace(JSON_EXTENSION, EMPTY_STRING) for file in sorted(listdir(PROFILES_PATH))]
 
-    ''' Prints the welcome message the user sees when they first start our application.
-        It will also print a list of profiles the user can load if there are any or
-        prompt the user to create a new one. '''
-    def _printWelcomeMessage(self):
-        twoPrintOutcomes(self._profiles, [WELCOME_MESSAGE, LOAD_CREATE_PROFILE_MESSAGE], \
-            [WELCOME_MESSAGE, NO_PROFILES_MESSAGE], [CREATE_PROFILE])
-
     def _redirect(self, user_input):
         pass
 
     ''' This method starts a session. It will greet the user and then prompt the user
         to enter a command that will be redirected. '''
     def start(self):
-        self._printWelcomeMessage()
+        twoPrintOutcomes(self._profiles, [WELCOME_MESSAGE, LOAD_CREATE_PROFILE_MESSAGE], \
+            [WELCOME_MESSAGE, NO_PROFILES_MESSAGE], [CREATE_PROFILE])
         while True:
             user_input = input(ENTER_COMMAND_MESSAGE)
             self._redirect(user_input)
